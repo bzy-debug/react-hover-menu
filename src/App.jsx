d84deb8e778d96d9646021d8e4ad303f313e377f
bzy-debug <bzy_sustech@foxmail.com>
2023-07-16 04:06:34 +0800

    Refactor

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,29 @@ import { useRef } from "react";
 function App() {
   const delayTimer = useRef(null);
   const menuRef = useRef(null);
+
+  function openMenu() {
+    clearTimeout(delayTimer.current);
+    menuRef.current.style.opacity = 1;
+    menuRef.current.style.display = "block";
+  }
+
+  function closeMenu() {
+    menuRef.current.style.opacity = 0;
+    delayTimer.current = setTimeout(
+      () => (menuRef.current.style.display = "none"),
+      150,
+    );
+  }
+
+  function toggleMenu() {
+    if (menuRef.current.style.opacity === "0") {
+      openMenu();
+    } else {
+      closeMenu();
+    }
+  }
+
   return (
     <div className="flex justify-between">
       {[1, 2, 3].map((i) => {
@@ -12,16 +35,13 @@ function App() {
         <button
           className="text-3xl font-bold underline"
           onMouseEnter={() => {
-            clearTimeout(delayTimer.current);
-            menuRef.current.style.opacity = 1;
-            menuRef.current.style.display = "block";
+            openMenu();
           }}
           onMouseLeave={() => {
-            menuRef.current.style.opacity = 0;
-            delayTimer.current = setTimeout(
-              () => (menuRef.current.style.display = "none"),
-              150,
-            );
+            closeMenu();
+          }}
+          onClick={() => {
+            toggleMenu();
           }}
         >
           Hello
@@ -30,16 +50,10 @@ function App() {
           ref={menuRef}
           className={`absolute right-0 top-full hidden w-max rounded-xl border p-3 opacity-0 shadow-menu transition-opacity`}
           onMouseEnter={() => {
-            clearTimeout(delayTimer.current);
-            menuRef.current.style.opacity = 1;
-            menuRef.current.style.display = "block";
+            openMenu();
           }}
           onMouseLeave={() => {
-            menuRef.current.style.opacity = 0;
-            delayTimer.current = setTimeout(
-              () => (menuRef.current.style.display = "none"),
-              150,
-            );
+            closeMenu();
           }}
         >
           <li className="px-3 text-center">Vite 3 Docs</li>
